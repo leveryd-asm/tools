3458c0914dcfc25ec17c945a5f15175f9edaf153
leveryd <leveryd@leveryd.com>
2022-11-22 22:22:20 +0800

    add: xray webhook and weixin

---
+++ xray-webhook/go.mod
@@ -0,0 +1,10 @@
+module xray-webhook
+
+go 1.18
+
+require github.com/tidwall/gjson v1.14.4
+
+require (
+	github.com/tidwall/match v1.1.1 // indirect
+	github.com/tidwall/pretty v1.2.0 // indirect
+)
---
+++ xray-webhook/main.go
@@ -0,0 +1,48 @@
+package main
+
+import (
+	"fmt"
+	"github.com/tidwall/gjson"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"os"
+	"strings"
+)
+
+// 企业微信机器人的webhook地址
+// https://developer.work.weixin.qq.com/document/path/91770
+var webhookURL = os.Getenv("WEBHOOK_URL")
+
+func sendToQYWX(msg string) {
+	content := fmt.Sprintf(`{"msgtype": "text", "text": {"content": "%s"}}`, msg)
+	_, err := http.Post(webhookURL, "application/json", strings.NewReader(content))
+	if err != nil {
+		log.Fatalln(err)
+	}
+}
+
+func xrayWebhook() func(http.ResponseWriter, *http.Request) {
+	return http.HandlerFunc(func(wrt http.ResponseWriter, req *http.Request) {
+		body, err := ioutil.ReadAll(req.Body)
+		if err != nil {
+			log.Fatalln(err)
+		}
+		// xray webhook data format: https://docs.xray.cool/#/webhook/vuln
+		value := gjson.Get(string(body), "data.detail")
+		msg := value.String()
+		sendToQYWX(msg)
+	})
+}
+
+func main() {
+	if webhookURL == "" {
+		log.Fatalln("env WEBHOOK_URL is empty")
+	}
+	// Add handle func for producer.
+	http.HandleFunc("/webhook", xrayWebhook())
+
+	// Run the web server.
+	fmt.Println("start producer-api ... !!")
+	log.Fatal(http.ListenAndServe(":8080", nil))
+}
